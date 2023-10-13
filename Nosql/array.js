927f7090ca0101fa7898118e3da82055d67a1511
YisethVelez000 <yisethvelez07@gmail.com>
2023-10-12 21:31:53 -0500

    tercer commit

---
+++ Nosql/array.js
@@ -0,0 +1,38 @@
+usuarios =[{
+    'idUsuario': 1,
+    'nombre': 'Juan',
+    'usuario': 'juanpro',
+    'contraseña': 'password'
+},
+{
+    'idUsuario': 2,
+    'nombre':'Pedro',
+    'contraseña': '1018227558'
+}
+]
+
+clientes = [{
+    "idCliente": 323,
+    "nombres":"Luis",
+    "apellidos":"Gonzalez",
+},
+{
+    'idCliente': 43424,
+    'nombres':'Maria',
+    'apellidos': 'Ruiz',
+    'direccion': '54b13',
+    'eps': 'savia'
+}
+]
+
+ordenProduccion[{
+    'idOrdenProd': 1,
+    'idEmpleado' : 344,
+    'fechaInicio': '2021-09-16'
+},
+{
+    'idOrdenProd': 2,
+    'idEmpleado': 434,
+    'fechaInicio' :'2022-06-03',
+    'fechaFin' : '2022-07-04'
+}]
---
+++ Nosql/arraycamila.js
@@ -0,0 +1,35 @@
+catalogoProductos =[{
+    'idCatalogo' : 3424,
+    'idProducto':1085679,
+    'nombre':'camisa',
+    'precioVenta': '$3.500'
+}, 
+{
+        'idCatalogo' : 3424,
+        'idProducto' :1085680,
+        'nombre':'pantalon',
+        'precioVenta': '$4.500',
+        'iva ': 19
+}]
+
+compras =[{
+    "cliente": "Juan",
+    "fechaCompra":"20/04/2021",
+    "productos": 'camisa',
+},
+{
+    'cliente' : 'sara',
+    'fechaCompra' : '20/12/2022',
+    'productos' : 'pantalon, buzo',
+    'total' : 55000
+}
+]
+
+cotizacion =[{
+    'cliente':"juan",
+    'descripcionCotizacion':"buzo",
+    },{
+        'cliente':"Sara",
+        'descripcionCotizacion':"Pantalón y camisa",
+        'direccionEntrega' : 'calle 80 # 54'
+}] 
